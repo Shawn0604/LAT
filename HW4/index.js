af15ab81fe4ca377f256106d672252e5d63c1f7e
Shawn0604 <shawn901011@gmail.com>
2023-05-16 00:04:54 +0800

    homework
    homework

---
+++ HW4/index.js
@@ -0,0 +1,134 @@
+'use strict';
+const line = require('@line/bot-sdk'),
+      express = require('express'),
+      axios = require('axios'),
+      configGet = require('config');//({
+//        path: '/Users/anderson/Desktop/Data Science/config/default.json' //指定檔案的絕對路徑
+//      });
+const {TextAnalyticsClient, AzureKeyCredential} = require("@azure/ai-text-analytics");
+
+//Line config
+const configLine = {
+  channelAccessToken:configGet.get("CHANNEL_ACCESS_TOKEN"),
+  channelSecret:configGet.get("CHANNEL_SECRET")
+};
+
+//Azure Text Sentiment
+const endpoint = configGet.get("ENDPOINT");
+const apiKey = configGet.get("TEXT_ANALYTICS_API_KEY");
+
+const client = new line.Client(configLine);
+const app = express();
+
+const port = process.env.PORT || process.env.port || 3001;
+
+app.listen(port, ()=>{
+  console.log(`listening on ${port}`);
+   
+});
+
+async function MS_TextSentimentAnalysis(thisEvent){
+    console.log("[MS_TextSentimentAnalysis] in");
+    const analyticsClient = new TextAnalyticsClient(endpoint, new AzureKeyCredential(apiKey));
+    let documents = [];
+    documents.push(thisEvent.message.text);
+    // documents.push("我覺得櫃檯人員很親切");
+    // documents.push("熱水都不熱，爛死了，很生氣！");
+    // documents.push("房間陳設一般般");
+    //const results = await analyticsClient.analyzeSentiment(documents);
+    const results = await analyticsClient.analyzeSentiment(documents,"zh-Hant",{
+      includeOpinionMining: true
+    });
+    //const results = await analyticsClient.AnalyzeSentimentOptions(documents);
+    console.log("[results] ", JSON.stringify(results));
+    //save to Json Server
+    let newData = {
+      "sentiment":results[0].sentiment,
+      "confidenceScore":results[0].confidenceScores[results[0].sentiment],
+      "opinionText":""
+    };
+
+    if (results[0].sentences[0].opinions.length!=0){
+      newData.opinionText = results[0].sentences[0].opinions[0].target.text;
+    }
+    let axios_add_data = {
+      method:"post",
+      url:"https://shawnchatbot.azurewebsites.net/reviews",
+      headers:{
+        "content-type":"application/json"
+      },
+      data:newData
+    };
+
+    axios(axios_add_data)
+    .then(function(response){
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function(){console.log("error");});
+
+
+    //回傳內容改變
+    const state = results[0].sentiment;
+    const score = results[0].confidenceScores[results[0].sentiment];
+    const object = newData.opinionText;
+
+    //If the model can't find the topic, then turn to the general answer
+    let object_adj = "";
+    if (object === ""){
+      object_adj = "相關"
+    } else {
+      object_adj = object
+    }
+    
+    //Use different answers according to the nature of the message
+    let feedbackType = '';
+    let feedback = '';
+    if (state === 'neutral') {
+      feedbackType='中性',
+      feedback = "試試看更正面的語氣吧~";
+    } else if (state === 'positive') {
+      feedbackType='正向',
+      feedback = "請繼續保持您正向的心態~";
+    } else if (state === 'negative') {
+      feedbackType='負面',
+      feedback = "請稍作休息，待情緒穩定再傳送訊息~";
+    }
+
+    //Give three different answer forms to meet assignment requirements
+    const echo1 = {
+      type: 'text',
+      text: state
+    };
+    const echo2 = {
+      type: 'text',
+      text: `您所輸入的訊息為${feedbackType}語氣，其${feedbackType}指數達 ${score}`
+    };
+    const echo3 = {
+      type: 'text',
+      text: `十分感謝您傳送${feedbackType}語氣的語句， 其指數達${score}，${feedback}`
+    };
+    return client.replyMessage(thisEvent.replyToken, [echo1,echo2,echo3]);
+
+
+}
+
+app.post('/callback', line.middleware(configLine),(req, res)=>{
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result)=>res.json(result))
+    .catch((err)=>{
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event){
+  if(event.type !== 'message' || event.message.type !== 'text'){
+    return Promise.resolve(null);
+  }
+
+  MS_TextSentimentAnalysis(event)
+    .catch((err) => {
+      console.error("Error:", err);
+    }); 
+}
---
+++ HW4/readme.md
@@ -0,0 +1,23 @@
+## HW4-Customer Review Sentiment Analysis Bot
+----
+### LINE BOT介面
+![擷取5](./pic/%E6%93%B7%E5%8F%965.PNG)
+
+### LINE BOT功能介紹：
+1. 屬性：正向/中性/負向
+2. 屬性+該屬性指數
+3. 屬性+該屬性指數+主詞判斷
+    - 正向
+    ![擷取1](./pic/%E6%93%B7%E5%8F%961.PNG)
+
+
+    - 中性
+    ![擷取2](./pic/%E6%93%B7%E5%8F%962.PNG)
+
+    - 負面
+    ![擷取3](./pic/%E6%93%B7%E5%8F%963.PNG)
+4. LINE BOT所輸出的內容自動儲存至JSON Server
+![擷取6](./pic/%E6%93%B7%E5%8F%966.PNG)
+
+5. LINE BOT依據使用者所輸出的內容進行判斷，並依屬性轉換成圓餅圖
+![擷取7](./pic/%E6%93%B7%E5%8F%967.png)
