8ef32900949fc66d784e091537b8c472a6d12f58
Shawn0604 <shawn901011@gmail.com>
2023-05-30 11:42:52 +0800

    0530
    0530

--- HW5/Samples/01.image-recognition/main.js
+++ HW5/Samples/01.image-recognition/main.js
@@ -9,12 +9,13 @@ function processImage() {
     
     //確認區域與所選擇的相同或使用客製化端點網址
     var url = "https://eastus.api.cognitive.microsoft.com/";
-    var uriBase = url + "vision/v2.1/analyze";
+    var uriBase = url + "vision/v2.1/describe";
     
     var params = {
-        "visualFeatures": "Categories,Description,Color",
-        "details": "",
-        "language": "en",
+        // "visualFeatures": "Faces,Brands,Adult,Categories,Description,Color",
+        // "details": "",
+        "maxCandidates":"10",
+        "language": "zh",
     };
     //顯示分析的圖片
     var sourceImageUrl = document.getElementById("inputImage").value;
@@ -34,8 +35,24 @@ function processImage() {
     .done(function(data) {
         //顯示JSON內容
         $("#responseTextArea").val(JSON.stringify(data, null, 2));
-        $("#picDescription").text();
-    })
+        $("#picDescription").empty();
+        // for (var x = 0; x < data.description.captions.length;x++){
+        //     $("#picDescription").append(data.description.captions[x].text + "<br>");
+        // }
+        // 根據可信度排序描述
+        var sortedCaptions = data.description.captions.sort(function(a, b) {
+            return b.confidence - a.confidence;
+        // for (var x = 0; x < data.description.captions.length;x++){
+        //     $("#picDescription").append(data.description.captions[x].text + "<br>");
+        // }
+        });
+
+        // 選擇最有最高可信度的一句話
+        var bestCaption = sortedCaptions[0].text;
+        $("#picDescription").append("<p style='font-size: 15px;'>依據測試的信心程度，該圖片最具有可信程度的描述為:</p>");//變更字體
+        $("#picDescription").append("<p style='font-size: 24px; color: #8B4513'>" + bestCaption + "</p>");//變更字體與顏色
+        })
+        // $("#picDescription").append("這裡有"+data.faces.length+"個人");
     .fail(function(jqXHR, textStatus, errorThrown) {
         //丟出錯誤訊息
         var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
